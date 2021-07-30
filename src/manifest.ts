310f93ad3cd97fb4be7c6ee9130654084f69c5ef
Ramiro Berrelleza <ramiro@okteto.com>
2021-07-30 06:56:14 -0700

    remove call to envsubst

--- src/manifest.ts
+++ src/manifest.ts
@@ -2,7 +2,6 @@ import {promises} from 'fs';
 import * as yaml from 'yaml';
 import * as path from 'path';
 import * as vscode from 'vscode';
-import envsubst from '@tuplo/envsubst';
 
 export class Manifest {
     constructor(public name: string, public namespace: string, public workdir: string) {}
@@ -21,11 +20,8 @@ export async function getManifest(manifestPath: string): Promise<Manifest> {
     }
 
     const j = parsed.toJSON();
-    const name = envsubst(j.name);
-    const namespace = envsubst(j.namespace);
-    const workdir = envsubst(j.workdir);
     
-    const m = new Manifest(name, namespace, workdir);
+    const m = new Manifest(j.name, j.namespace, j.workdir);
     if (!m.name){
         throw new Error(`${manifestPath} is not a valid Okteto manifest`);
     }
