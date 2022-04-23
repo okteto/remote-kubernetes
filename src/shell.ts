db9cb8e16245998472e0d27cd0b84f856f0d739c
Ramiro Berrelleza <ramiro@okteto.com>
2022-04-22 22:56:33 -0700

    check the default profile

--- src/shell.ts
+++ src/shell.ts
@@ -9,42 +9,54 @@ const IS_COMMAND = /(cmd.exe$|cmd$)/i;
 //const IS_POWERSHELL_CORE = /(pwsh.exe$|pwsh$)/i;
 
 
-function getTerminalShellPath(): string | undefined {
-    const shellConfig = vscode.workspace.getConfiguration('terminal.integrated.shell');
-    terminal.external.windowsExec
-    let osType = "";
-    switch(os.platform()){
-        case "win32":
-            osType = "windows";
-            break;
-        case "darwin":
-            osType = "osx";
-            break;
-        case "linux":
-            osType = "linux";
-            break;
-    }
-    return shellConfig.get<string>(osType)!;    
+type vscodeProfile = {
+    path: string[] | undefined,
 }
 
-export function isGitBash(): boolean {
-    if (os.platform() !== "win32") {
-        return false;
-    }
-
-    const sh = getTerminalShellPath()
-    if (sh === undefined) {
-        return false;
-    }
-
-    return IS_GITBASH.test(sh)
+function getDefaultProfileName(): string | undefined {
+    const profile = vscode.workspace.getConfiguration('terminal.integrated.defaultProfile');
+    const winProfile = profile.get<string>('windows');
+    return winProfile;
 }
 
+function getProfile(name: string): vscodeProfile | undefined {
+    const profiles = vscode.workspace.getConfiguration('terminal.integrated.profiles');
+    const winProfiles = profiles.get<Map<string, vscodeProfile>>('windows');
+    if (winProfiles === undefined) {
+        return undefined;
+    }
+
+    return winProfiles.get(name);
+}
+// rules
+// 1. check if there's a default profile
+// 2. check terminal.external.windowsExec
+// 3. assume powershell?
 export function isWindowsCmd(): boolean {
     if (os.platform() !== "win32") {
         return false;
     }
 
+    const winProfile = getDefaultProfileName();
+    if (winProfile) {
+        if (winProfile === "PowerShell") {
+            return false;
+        }
+    
+        if (winProfile === "Command Prompt") {
+            return true;
+        }
+
+        const defaultProfile = getProfile(winProfile);
+        if (defaultProfile && defaultProfile.path) {
+            defaultProfile.path.forEach(p => {
+                if (p && IS_COMMAND.test(p)) {
+                    return true;
+                }
+            });
+        }
+    }
+    
     const shellConfig = vscode.workspace.getConfiguration('terminal.external');
     const sh = shellConfig.get<string>('windowsExec');
     if (sh === undefined) {
@@ -52,5 +64,4 @@ export function isWindowsCmd(): boolean {
     }
 
     return IS_COMMAND.test(sh)
-    
 }
