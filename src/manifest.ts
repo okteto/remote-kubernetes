f6f057fd2da35e0d6a1a5806d50d31f570ffd12e
Ramiro Berrelleza <rberrelleza@gmail.com>
2022-07-19 19:21:10 -0700

    split into two functions
    Signed-off-by: Ramiro Berrelleza <rberrelleza@gmail.com>

--- src/manifest.ts
+++ src/manifest.ts
@@ -7,17 +7,7 @@ export class Manifest {
     constructor(public name: string, public namespace: string, public workdir: string, public port: number) {}
 }
 
-export async function getManifests(manifestPath: string): Promise<Manifest[]> {
-    const data = await promises.readFile(manifestPath, {encoding: 'utf8'});
-    if (!data) {
-        throw new Error(`${manifestPath} is not a valid Okteto manifest`);
-    }
-
-    const parsed = yaml.parseDocument(data);
-    if (parsed.errors && parsed.errors.length > 0) {
-        console.error(`${manifestPath} is not a valid yaml file: ${parsed.errors.join(", ")}`);
-        throw new Error(`${manifestPath} is not a valid yaml file`);
-    }
+export function parseManifest(parsed: yaml.Document.Parsed): Manifest[] {
     const j = parsed.toJSON();
     const result: Array<Manifest> = [];
 
@@ -29,12 +19,28 @@ export async function getManifests(manifestPath: string): Promise<Manifest[]> {
     } else {
         const m = new Manifest(j.name, j.namespace, j.workdir, j.remote);
         if (!m.name){
-            throw new Error(`${manifestPath} is not a valid Okteto manifest`);
+            throw new Error(`Invalid manifest`);
         }
         result.push(m);
     }
 
     return result;
+}
+
+export async function getManifests(manifestPath: string): Promise<Manifest[]> {
+    const data = await promises.readFile(manifestPath, {encoding: 'utf8'});
+    if (!data) {
+        throw new Error(`${manifestPath} is not a valid Okteto manifest`);
+    }
+
+    const parsed = yaml.parseDocument(data);
+    if (parsed.errors && parsed.errors.length > 0) {
+        console.error(`${manifestPath} is not a valid yaml file: ${parsed.errors.join(", ")}`);
+        throw new Error(`${manifestPath} is not a valid yaml file`);
+    }
+    
+    return parseManifest(parsed);
+    
 }
 
 export function getDefaultLocation(): vscode.Uri | undefined{
